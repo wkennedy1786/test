9beb217d5ad756e1093288fca49574b089958856
William Kennedy <wkennedy@dublinschool.org>
2019-02-10 14:17:31 +0000

    Updated for Test Challenge 4

--- test.c
+++ test.c
@@ -1,6 +1,26 @@
-//This is only a test....
+// Test Chapter 4 Challenge
+// You got this by "git clone https://github.com/wkennedy1786/test"
+// Collaborate to fill in the blanks
+// Commit and push your work to github
+// Dr. Bill, Feb. 2019
+
+#include <cs50.h>
 #include <stdio.h>
+
+struct student
+{
+     int id;
+     char student_name[30];
+     float exam_score;
+};
+
 int main(void) 
 {
-  printf("Hello, world!\n");
+ // TO DO
+ // Read Students.txt into a struct
+ // Note that the first character in the file is the number of student entries
+ 
+ // TO DO
+ // Reiteratively, get a student ID #
+ // printf the respective student's name and exam score
 }
