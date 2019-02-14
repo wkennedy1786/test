020bd4a1a1c4918c375fab733df668934460f0d2
Oliver Tullio <o_tullio@dublinschool.org>
2019-02-14 20:40:50 +0000

    Added path

--- test.c
+++ test.c
@@ -6,6 +6,7 @@
 
 #include <cs50.h>
 #include <stdio.h>
+#define FILE "students.txt"
 
 struct student
 {
@@ -14,12 +15,24 @@ struct student
      float exam_score;
 };
 
-int main(void) 
+int main(void)
 {
+    // determine path to dictionary
+    string students = dirname(argv[0]);
+    char path[strlen(students) + 1 + strlen(students) + 1];
+    sprintf(path, "%s/%s", directory, FILE);
+
+    // load structure
+    if (!load(path))
+    {
+        printf("Could not open student list.\n");
+        return 1;
+    }
+
  // TO DO
  // Read Students.txt into a struct
  // Note that the first character in the file is the number of student entries
- 
+
  // TO DO
  // Reiteratively, get a student ID #
  // printf the respective student's name and exam score
