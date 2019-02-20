ae4962bfac949e3b95c4ef0a6c58ec1113bd56fa
wkennedy1786 <wkennedy@dublinschool.org>
2019-02-20 09:12:59 -0500

    Update test.c

--- test.c
+++ test.c
@@ -17,24 +17,6 @@ struct student
 
 int main(int argc; string argv[])
 {
-    // remember filenames
-    char *infile = argv[1];
-
-    // open input file
-    FILE *inptr = fopen(infile, "r");
-    if (inptr == NULL)
-    {
-        printf("Could not open %s.\n", infile);
-        return;
-    }
-
-    // read infile's length
-    BITMAPFILEHEADER bf;
-    fread(void* FILE, sizeof(BITMAPFILEHEADER), 1, inptr);
-
-
-
-
  // TO DO
  // Read Students.txt into a struct
  // Note that the first character in the file is the number of student entries
